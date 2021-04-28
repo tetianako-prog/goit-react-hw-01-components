30db27d19b16de1133d98f7f3ad82f4a70244ac9
tetianako-prog <t.kobyliakova@gmail.com>
2021-04-28 10:44:14 +0300

    Improve file structure

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/Transactions/TransactionHistory';
 
-import user from './user.json';
-import statisticalData from './statistical-data.json';
-import friends from './friends.json';
-import transactions from './transactions.json';
+import { user, statisticalData, friends, transactions } from './data';
 
 const App = () => (
   <div className={'container'}>
---
+++ src/data/index.js
@@ -0,0 +1,6 @@
+import user from './user.json';
+import statisticalData from './statistical-data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
+
+export { user, statisticalData, friends, transactions };
